4817cc1662c8276a1317f8a9b4a831fccd41492c
yura <pekarskyi1054@gmail.com>
2022-06-29 17:24:55 +0300

    Finished zero automation, rewritten code for better efficiency. Committer: PekarskyiYV@dtek.com

--- eaglegaze_kiwi_margin_cost/main.py
+++ eaglegaze_kiwi_margin_cost/main.py
@@ -1,5 +1,7 @@
 from margin_cost import Margin_cost
+from margin_cost_2 import MarginCost
 
 if __name__ == '__main__':
-    mc = Margin_cost()
-    mc.run_all_scenario()
+    # mc = Margin_cost()
+    # mc.run_all_scenario()
+    MarginCost().run()
